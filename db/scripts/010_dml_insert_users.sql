33c64422b70cb2ab8098bf0bfb2948827cfa44a3
digest15 <digest15@yandex.ru>
2023-04-29 19:34:10 +0800

    #2094
    Use database for Spring Security

---
+++ db/scripts/010_dml_insert_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password) VALUES ('user', '$2a$10$N5Zd5u.OQl5Acb4fsHbgeOMojWJQnG7mazH7f.48OAo0CzywFeCx2');
+INSERT INTO authorities (username, authority) VALUES ('user', 'ROLE_USER');
